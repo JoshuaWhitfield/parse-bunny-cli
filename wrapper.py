bcdb5baa050928ca36482d72988a70873c8dd21c
Joshua Whitfield <joshuawhitfield022@gmail.com>
2025-04-15 22:05:33 -0700

    cleaning up code base

--- wrapper.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the current directory to the path
-script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, script_dir)
-
-# Import and run the main script
-import main
